fc7658032df89e9f66d3f814b4828253762bd336
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:24:40 +0530

    Initial commit

---
+++ circle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcparameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            circle circle = new circle();
+            Console.WriteLine("enter radius:");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(circle.Calculate(x => 2 * (p+p*p)));
+            Console.WriteLine(circle.Calculate(r => 2 * Math.PI * p));
+            Console.WriteLine();
+            Console.WriteLine(circle.Calculate(r =>  Math.PI * p));
+            Console.WriteLine(circle.Calculate(a =>  p * p * p));
+            Console.WriteLine("enter a string");
+            string s = Console.ReadLine();
+            Console.WriteLine(circle.Changecase(a => s.ToUpper()));
+            //Console.Writeline("Enter upper case letters");
+            string t = "IN UPPER CASE";
+            Console.WriteLine(circle.Changecase(x => t.ToLower()));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ circle/circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcparameter
+{
+    class circle
+    {
+        private double radius;
+        string st;
+
+        public double Calculate(Func<double,double>mp)
+        {
+            return mp(radius);
+        }
+
+        public string Changecase(Func<string,string> uc)
+        {
+            return uc(st);
+        }
+
+    }
+}
